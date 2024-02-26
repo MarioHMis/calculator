2060c033ae4926ce9b7ef9531e717c828ea64599
Mariohmis <mariohmis@gmail.com,noreply.github.com>
2024-02-26 12:22:52 -0600

    Chenge button color, init jS

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const screen = document.querySelector('.screen');
+const buttons = document.querySelectorAll('.btn');
 
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const pushbutton = button.textContent;
+
+    if (button.id === 'clear') {
+      screen.textContent = '0';
+      return;
+    }
+
+    if (button.id === 'delete') {
+      if (screen.textContent.length === 1) {
+        screen.textContent = '0';
+      } else {
+        screen.textContent = screen.textContent.slice(0, -1);
+      }
+
+      return;
+    }
+
+    if (button.id === 'equal') {
+      try {
+        screen.textContent = eval(screen.textContent);
+      } catch {
+        screen.textContent = 'Error';
+      }
+
+      return;
+    }
+
+    if (screen.textContent === '0') {
+      screen.textContent = pushbutton;
+    } else {
+      screen.textContent += pushbutton;
+    }
+  });
+});
